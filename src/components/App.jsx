86cc2fb95be3d12090c8fee8caf22fce2f658ef2
AntonKobyshev <antonkobyshev@gmail.com>
2023-06-12 18:58:28 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { Title, Subtitle, Container } from './App.styled';
+import { AiFillContacts, AiFillBook } from 'react-icons/ai';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Title>
+        <AiFillBook size="36" />
+        Phonebook
+      </Title>
+      <ContactForm />
+
+      <Subtitle>
+        <AiFillContacts size="36" />
+        Contacts
+      </Subtitle>
+      <Filter />
+      <ContactList />
+    </Container>
   );
 };
