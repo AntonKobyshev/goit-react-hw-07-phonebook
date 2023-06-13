b913c20ffe71ba6717ac8e3fa7617bb6a3c8c45c
AntonKobyshev <antonkobyshev@gmail.com>
2023-06-13 16:09:33 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,18 @@
-import { ColorRing } from 'react-loader-spinner';
+import { Grid } from 'react-loader-spinner';
 import { Container } from './Loader.styled';
 
 export const Loader = () => {
   return (
     <Container>
-      <ColorRing
+      <Grid
+        height="80"
+        width="80"
+        color="#386b3c"
+        ariaLabel="grid-loading"
+        radius="12.5"
+        wrapperStyle={{}}
+        wrapperClass=""
         visible={true}
-        height="180"
-        width="180"
-        ariaLabel="blocks-loading"
-        wrapperStyle={{ justifyContent: 'center' }}
-        wrapperClass="blocks-wrapper"
-        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
       />
     </Container>
   );
